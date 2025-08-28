ea10ce6a5fe37e37fbb911016b315b4e01ce59ef
Tushar <tusharrshende@gmail.com>
2025-08-28 19:28:47 +0530

    new update

--- app.py
+++ app.py
@@ -38,7 +38,7 @@ def get_prediction():
     baby_df = pd.DataFrame(baby_data_cleaned)
 
     #load machine learning trained model
-    with open("model/model.pkl", "rb") as obj:
+    with open("model.pkl", "rb") as obj:
         model = pickle.load(obj)
 
     #make predictions on user data
